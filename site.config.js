74600fbc8fcfad09cf973345d53f258789f31e73
ancientmystic-dev <68768301+ancientmystic-dev@users.noreply.github.com>
2021-12-08 16:37:59 +0530

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '9750ae31106344c0b7e16c621cb96a6d',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
+  name: 'Kumar R',
   domain: 'transitivebullsh.it',
   author: 'Travis Fischer',
 
